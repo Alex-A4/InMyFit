f970d7df1f8ed13026d60581989a3005469640f7
Alex <alex.A4.25@yandex.ru>
2019-04-05 16:13:02 +0300

    Added theme

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,23 @@
 import 'package:flutter/material.dart';
-import 'package:inmyfit/ui/app.dart';
-
+import 'package:flutter/services.dart';
+import 'package:inmyfit/src/app.dart';
 
 void main() {
-  runApp(App());
+  SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+    statusBarColor: Colors.transparent,
+    statusBarIconBrightness: Brightness.light,
+    statusBarBrightness: Brightness.dark,
+  ));
+  runApp(MaterialApp(
+    title: 'inmyfit',
+    theme: theme,
+    home: App(),
+  ));
 }
+
+final theme = ThemeData(
+  primaryColor: Color(0xFF34EBE9),
+  primaryColorLight: Color(0xFFD0FFAE),
+  brightness: Brightness.light,
+  textTheme: TextTheme(title: TextStyle(fontSize: 22.0, color: Colors.white)),
+);
