3b732da963451405b7aa7f440e77837148168133
Alex <alex.A4.25@yandex.ru>
2019-04-12 21:10:16 +0300

    Changed calendar_carousel to table_calendar

--- lib/src/ui/activity/calendar_widget.dart
+++ lib/src/ui/activity/calendar_widget.dart
@@ -1,48 +1,82 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_calendar_carousel/flutter_calendar_carousel.dart';
 import 'package:flutter_redux/flutter_redux.dart';
 import 'package:inmyfit/main.dart';
 import 'package:inmyfit/src/redux/activity_redux.dart';
+import 'package:intl/intl.dart';
+import 'package:table_calendar/table_calendar.dart';
 
 class CalendarWidget extends StatelessWidget {
+  DateTime _selectedDate;
+
   CalendarWidget({Key key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    return StoreConnector<ActivityState, Function(DateTime, List<dynamic>)>(
+    return StoreConnector<ActivityState, Function(DateTime)>(
       converter: (store) {
-        return (date, list) => store.dispatch(FetchDataAction(date));
+        return (date) => store.dispatch(FetchDataAction(date));
       },
       builder: (BuildContext context, callback) {
         return Container(
-          key: Key('CalendarKey'),
-          height: 120.0,
-          child: CalendarCarousel(
-            weekFormat: true,
-            isPrimary: false,
-            locale: "ru",
-            childAspectRatio: 1.5,
-            isScrollable: false,
-            weekdayTextStyle:
-                TextStyle(color: Colors.grey[500], fontSize: 12.0),
-            weekDayMargin: EdgeInsets.all(0.0),
-            headerMargin: EdgeInsets.only(top: 16.0),
-            todayButtonColor: Colors.transparent,
-            iconColor: Colors.grey[500],
-            selectedDayButtonColor: theme.primaryColorLight,
-            showWeekDays: true,
-            headerTextStyle: TextStyle(color: Colors.grey[500], fontSize: 24.0),
-            prevDaysTextStyle: dayTextStyle,
-            nextDaysTextStyle: dayTextStyle,
-            daysTextStyle: dayTextStyle,
-            weekendTextStyle: dayTextStyle,
-            selectedDayTextStyle: dayTextStyle,
-            todayTextStyle: dayTextStyle.copyWith(color: theme.primaryColor),
-            onDayPressed: callback,
+          child: TableCalendar(
+            selectedDay: _selectedDate,
+            onDaySelected: (date, list) {
+              _selectedDate = date;
+              callback(date);
+            },
+            calendarStyle: CalendarStyle(
+              weekendStyle: dayTextStyle,
+              selectedColor: theme.primaryColor,
+              todayStyle: dayTextStyle.copyWith(color: theme.primaryColor),
+              todayColor: Theme
+                  .of(context)
+                  .scaffoldBackgroundColor,
+              weekdayStyle: dayTextStyle,
+              outsideDaysVisible: false,
+              selectedStyle: dayTextStyle.copyWith(color: Colors.white),
+            ),
+            headerStyle: HeaderStyle(
+              formatButtonColor: greyColor,
+              formatButtonSize: 35.0,
+              titleTextStyle: dayTextStyle,
+              titleTextBuilder: (date, locale) {
+                String dateStr = DateFormat.yMMMM(locale).format(date).trim();
+                dateStr = dateStr.replaceRange(dateStr.length-3, dateStr.length, '');
+                var first= dateStr.substring(0, 1);
+                dateStr = dateStr.replaceAll(first, first.toUpperCase());
+                return dateStr;
+              },
+              centerHeaderTitle: true,
+              leftChevronIcon: Icon(
+                Icons.keyboard_arrow_left,
+                size: 30.0,
+                color: greyColor,
+              ),
+              rightChevronIcon: Icon(
+                Icons.chevron_right,
+                size: 30.0,
+                color: greyColor,
+              ),
+              leftChevronPadding: EdgeInsets.all(0.0),
+              rightChevronPadding: EdgeInsets.all(0.0),
+            ),
+            availableCalendarFormats: {
+              CalendarFormat.month: Icons.arrow_drop_down,
+              CalendarFormat.week: Icons.arrow_drop_up,
+            },
+            daysOfWeekStyle: DaysOfWeekStyle(
+                weekdayStyle: dayTextStyle.copyWith(fontSize: 15.0),
+                weekendStyle: dayTextStyle.copyWith(fontSize: 15.0)),
+            availableGestures: AvailableGestures.horizontalSwipe,
+            startingDayOfWeek: StartingDayOfWeek.monday,
+            initialCalendarFormat: CalendarFormat.week,
+            locale: "ru_RU",
           ),
         );
       },
     );
   }
 
-  final dayTextStyle = TextStyle(color: Colors.grey[500], fontSize: 24.0);
+  static final greyColor = Colors.grey[500];
+  final dayTextStyle = TextStyle(color: greyColor, fontSize: 24.0, fontFamily: 'ProstoSans');
 }
