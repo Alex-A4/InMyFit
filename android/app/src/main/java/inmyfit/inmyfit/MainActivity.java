ecaa9476a49a50cb9a7430a034f22ca62a435320
Alex <alex.A4.25@yandex.ru>
2019-04-19 15:16:47 +0300

    Added lib to debug DB

--- android/app/src/main/java/inmyfit/inmyfit/MainActivity.java
+++ android/app/src/main/java/inmyfit/inmyfit/MainActivity.java
@@ -1,6 +1,9 @@
 package inmyfit.inmyfit;
 
 import android.os.Bundle;
+
+import com.amitshekhar.DebugDB;
+
 import io.flutter.app.FlutterActivity;
 import io.flutter.plugins.GeneratedPluginRegistrant;
 
@@ -9,5 +12,6 @@ public class MainActivity extends FlutterActivity {
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     GeneratedPluginRegistrant.registerWith(this);
+    DebugDB.getAddressLog();
   }
 }
