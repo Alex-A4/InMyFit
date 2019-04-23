9e900da763f85dfc2349cb7dcc3431540b0d46ab
Alex <alex.A4.25@yandex.ru>
2019-04-23 19:09:47 +0300

    Move out initializing of app to own function

--- lib/src/bloc/menu/bloc_menu.dart
+++ lib/src/bloc/menu/bloc_menu.dart
@@ -1,6 +1,7 @@
 import 'package:bloc/bloc.dart';
 import 'package:inmyfit/src/controller/current_activity_controller.dart';
 import 'package:inmyfit/src/controller/day_activity_controller.dart';
+import 'package:inmyfit/src/models/tablet_intake.dart';
 import 'package:inmyfit/src/models/water_intake.dart';
 import 'menu.dart';
 import 'package:redux/redux.dart';
@@ -15,30 +16,7 @@ class MenuBloc extends Bloc<MenuEvent, MenuState> {
   @override
   Stream<MenuState> mapEventToState(MenuEvent event) async* {
     if (event is EventMenuStarted) {
-      var date = DayActivityController.getPrimitiveDate(DateTime.now());
-
-      ///Initialize controllers for ActivityRedux
-      var currentController =
-          await CurrentActivityController.restoreFromCache();
-      var intakes = await readDayIntakes(date);
-      if (intakes[1] == null)
-        intakes[1] = WaterIntake.initOnBasic(currentController.water);
-
-      var dayController = DayActivityController(
-        date,
-        tablets: intakes[0],
-        water: intakes[1],
-      );
-
-      activityStore = Store<ActivityState>(
-        activityReducer,
-        initialState: ActivityState(
-          dayActivityController: dayController,
-          currentActivityController: currentController,
-        ),
-        middleware: [waterActionMiddleware, tabletsActionMiddleware],
-      );
-
+      await initializeActivity();
       yield StateActivityLog(activityStore);
     }
 
@@ -51,6 +29,45 @@ class MenuBloc extends Bloc<MenuEvent, MenuState> {
     if (event is EventProfile) yield StateProfile();
 
     if (event is EventShop) yield StateShop();
+  }
+
+  /// Initialize activity store by reading all from DB and cache
+  void initializeActivity() async {
+    var date = DayActivityController.getPrimitiveDate(DateTime.now());
+
+    ///Initialize controllers for ActivityRedux
+    var currentController = await CurrentActivityController.restoreFromCache();
+    var intakes = await readDayIntakes(date);
+
+    /// Initialize tablets
+    var tablets = intakes[0];
+    if (tablets.isEmpty) {
+      currentController.tablets.forEach((interval, tablet) {
+        if (interval.isContainsDate(DateTime.now()))
+          tablets.add(TabletsIntake.initOnBasic(tablet));
+      });
+    }
+
+    /// Initialize water
+    if (intakes[1] == null)
+      intakes[1] = WaterIntake.initOnBasic(currentController.water);
+
+    var dayController = DayActivityController(
+      date,
+      tablets: tablets,
+      water: intakes[1],
+    );
+    dayController =
+        checkAndMergeDayAndCurrentActivities(dayController, currentController);
+
+    activityStore = Store<ActivityState>(
+      activityReducer,
+      initialState: ActivityState(
+        dayActivityController: dayController,
+        currentActivityController: currentController,
+      ),
+      middleware: [waterActionMiddleware, tabletsActionMiddleware],
+    );
   }
 
   @override
